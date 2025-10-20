e11b59b5cdb207e99fb1fc2e7d43d766b6d7a5cf
sgawarat <sgawarat@gmail.com>
2025-10-20 11:32:37 +0900

    og画像生成を一旦止める

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -1,9 +1,14 @@
 import type { APIRoute } from "astro";
-import { generateOgImageForSite } from "@/utils/generateOgImages";
+// import { generateOgImageForSite } from "@/utils/generateOgImages";
 
 export const GET: APIRoute = async () => {
-  const buffer = await generateOgImageForSite();
-  return new Response(new Uint8Array(buffer), {
-    headers: { "Content-Type": "image/png" },
+  return new Response(null, {
+    status: 404,
+    statusText: "Not found",
   });
+
+  // const buffer = await generateOgImageForSite();
+  // return new Response(new Uint8Array(buffer), {
+  //   headers: { "Content-Type": "image/png" },
+  // });
 };
