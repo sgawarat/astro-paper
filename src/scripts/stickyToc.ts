0cffdda56d4c95131a54eefb044e2be09d82cbec
sgawarat <sgawarat@gmail.com>
2025-11-30 16:22:52 +0900

    ページ遷移でスクリプトを再実行するように修正する

---
+++ src/scripts/stickyToc.ts
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+document.addEventListener("astro:page-load", () => {
+  // headingクラスのsectionが画面内にあると、対応するTOC項目をアクティブにする
+  const observer = new IntersectionObserver(
+    (entries) => {
+      for (const entry of entries) {
+        const id = entry.target.getAttribute("aria-labelledby");
+        if (id === null) continue;
+
+        const e = document.querySelector(`#sticky-toc a[href="#${id}"]`);
+        if (e === null) continue;
+
+        if (entry.isIntersecting) {
+          e.classList.add("border-l-2");
+        } else {
+          e.classList.remove("border-l-2");
+        }
+      }
+    },
+    {
+      rootMargin: "0% 0px 0% 0px",
+    },
+  );
+  for (const h of document?.querySelectorAll("article section.heading") ?? []) {
+    observer.observe(h);
+  }
+});
