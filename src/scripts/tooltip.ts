20d9ec5edaba38091be6e2b9fc7062d7ee34ffd8
sgawarat <sgawarat@gmail.com>
2025-12-06 00:41:24 +0900

    タッチ操作でツールチップが画面外に出ることがある不具合を修正する

--- src/scripts/tooltip.ts
+++ src/scripts/tooltip.ts
@@ -2,29 +2,28 @@
 document.addEventListener("astro:page-load", () => {
   // ツールチップが画面外に出ないように位置を調整するイベントリスナーを追加する
   for (const tooltip of document.querySelectorAll<HTMLElement>(".tooltip")) {
-    function listener(this: HTMLElement) {
+    function listener(this: HTMLElement, event: MouseEvent | TouchEvent) {
       const content = this.querySelector<HTMLElement>(".tooltip-content");
       if (content === null) return;
 
       // 初期位置でのツールチップの矩形を調べる
-      content.style.maxWidth = "80ch";
+      if (event.type === "touchstart") {
+        content.style.display = "block";
+      }
       content.style.left = "50%";
-      content.style.right = "auto";
       content.style.transform = "translateX(-50%)";
-      let rect = content.getBoundingClientRect();
-
-      // ツールチップが画面に収まらないなら、小さくする
-      if (rect.width > document.documentElement.clientWidth) {
-        content.style.maxWidth = `${document.documentElement.clientWidth}px`;
-        rect = content.getBoundingClientRect();
+      const left = content.offsetLeft;
+      const width = content.offsetWidth;
+      if (event.type === "touchstart") {
+        content.style.display = "";
       }
 
       // ツールチップが画面からはみ出さないようにずらす
-      if (rect.left < 0) {
+      if (left < 0) {
         content.style.left = `${-tooltip.offsetLeft}px`;
         content.style.transform = "none";
-      } else if (rect.right > document.documentElement.clientWidth) {
-        content.style.left = `${document.documentElement.clientWidth - tooltip.offsetLeft - rect.width}px`;
+      } else if (left + width > document.documentElement.clientWidth) {
+        content.style.left = `${document.documentElement.clientWidth - tooltip.offsetLeft - width}px`;
         content.style.transform = "none";
       }
     }
