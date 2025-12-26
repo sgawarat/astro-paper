ca171f21647e8b146607da07bbf983147f01fd7d
sgawarat <sgawarat@gmail.com>
2025-12-26 17:11:09 +0900

    見出しへのリンクをremarkで生成するように変更する

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,7 @@ import mdx from "@astrojs/mdx";
 import { rehypeHeadingIds, type RemarkPlugin } from "@astrojs/markdown-remark";
 import remarkSectionize from "remark-sectionize";
 import rehypeExternalLinks from "rehype-external-links";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // 処理中のファイルのパスをログに出力するremarkプラグイン
 const remarkLog: RemarkPlugin<[]> = () => (_tree, vfile) => {
@@ -63,6 +64,23 @@ export default defineConfig({
     },
     rehypePlugins: [
       rehypeHeadingIds,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: { "aria-hidden": true },
+            children: [{ type: "text", value: "#" }],
+          },
+          headingProperties: { class: "group" },
+          properties: {
+            class:
+              "heading-link heading-link ms-2 no-underline opacity-75 md:opacity-0 md:group-hover:opacity-100 md:focus:opacity-100",
+          },
+        },
+      ],
       [rehypeKatex, { output: "mathml" }],
       [
         rehypeExternalLinks,
